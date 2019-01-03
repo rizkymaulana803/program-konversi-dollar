9b283f45d362a907f4f68494b931e2a4a997b0d6
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-01-03 18:50:56 +0700

    Add files via upload

---
+++ RIZKY MAULANA 311810588 program 8 konnvers dollar ke rupiah.cpp
@@ -0,0 +1,14 @@
+	#include <iostream>
+	using namespace std;
+	
+	int main ()
+	{
+		int Rp,Dollar;
+		cout <<"Masukan Nilai Dollar ayng ingin di konvers ke Rupiah : ";
+		cin >>Dollar;
+		
+		Rp = 15149 * Dollar;
+		cout <<"Hasil konvers Dollar ke Rupiah adalah = " <<Rp;
+		
+		return 0;
+	}
